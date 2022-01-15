ac2efceee4b650725099cf4cf3b4a286a171617a
Christian Dobert <thedondope@hey.com>
2022-01-15 19:33:48 +0100

    feat: add initial workflow

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -11,29 +11,39 @@ const (
 	correct = "🟩"
 	wrong = "🟨"
 	nospot = "⬛"
+	gotd = "ALTER"
+	cr = "\033[0;0H"
 )
 
+// Guess ...
 type Guess struct {
-	Next *Guess
-	Value interface{}
+	Value string
+	Evaluation string
 }
 
 func main() {
-	//var first *Guess
-	//guesses := [6]*Guess{}
-	//\033[0;0H
+	guesses := [6]*Guess{}
 
 	input := textinput.New("Enter 5 characters")
-	input.InitialValue = "⬛⬛⬛⬛⬛"
-	input.Placeholder = "Your need exactly 5 characters"
-
-	guess, err := input.RunPrompt()
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-
-		os.Exit(1)
+	input.InitialValue = ""
+	input.Placeholder = "You need exactly 5 characters"
+	input.Validate = func(value string) bool {
+		 return len(value) <=5
 	}
 
-	// do something with the result
-	_ = guess
+	for try:=0; try<6; try++ {
+		val, err := input.RunPrompt()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+
+			os.Exit(1)
+		}
+		g := &Guess {
+			Value: val,
+			Evaluation: "⬛⬛⬛⬛⬛",
+		}
+		fmt.Printf("%v %v\n", g.Value, g.Evaluation)
+		guesses[try] = g
+	}
+
 }
