fa0c2a32d87b9bef73d54ca93883b2cffe48a380
TheDonDope <thedondope@hey.com>
2022-01-15 05:26:53 +0100

    fix: remove unused auth0 code

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -4,42 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var (
-	audience string
-	domain   string
-)
-
-// CORSMiddleware returns the Handler Function for the CORS configuration.
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, POST, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 // NewServer ...
 func NewServer() *gin.Engine {
-	setAuthVariables()
-	r := gin.Default()
-	r.Use(CORSMiddleware())
-
-	baseURL := r.Group("/gordle")
-	// authorized.Use(AuthRequired())
+	s := gin.Default()
+	baseURL := s.Group("/gordle")
 	baseURL.GET("healthy", Healthy)
 
-	return r
-}
-
-// SetAuthVariables reads the environment variables
-func setAuthVariables() {
-	audience = "http://gordle-api" // FIXME: os.Getenv("AUTH0_API_IDENTIFIER")
-	domain = "localhost:4242"      // FIXME: os.Getenv("AUTH0_DOMAIN")
+	return s
 }
